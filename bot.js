e365f61995082f0b7fa02d6eb6e61fc2ab0c8b17
nkt142160-debug <n.kt142160@gmail.com>
2025-09-20 21:56:29 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 // ========== CONFIG ==========
-const TOKEN = "MTQxNzc0NjEzOTY0MTU0NDgzOA.G0oVPi.mMnUg53achqiS7prxcAIGCrlU5PCzMOBbZ44Zg";           // <-- dán token của bạn vào đây
+const TOKEN = "MTQxNzc0NjEzOTY0MTU0NDgzOA.GC7wLZ.aBOCrcV0PcrBG6ovW8xsN5uXerfx3fs762NiBM";           // <-- dán token của bạn vào đây
 const CHANNEL_ID = "1417744609731154000"; // <-- dán ID kênh bot hoạt động
 const ADMIN_ID = "1177116381897039906";     // <-- dán ID Discord của admin
 
@@ -328,4 +328,5 @@ client.on("messageCreate", async (message) => {
 // ===== start bot
 client.login(TOKEN).catch(err => {
   console.error("Không thể login bot — kiểm tra TOKEN:", err);
+
 });
